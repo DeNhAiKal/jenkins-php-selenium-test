c9dc20e5e4f81b5e7fceefad9f95f22331457fe2
DeNhAiKal <115686752+DeNhAiKal@users.noreply.github.com>
2024-07-22 21:27:09 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -x
 docker rm -f my-apache-php-app || true
-docker run -d -p 80:80 --name my-apache-php-app -v ${WORKSPACE}/src:/var/www/html php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v C:\Users\denpe\Documents\Github\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
 sleep 5
 CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' my-apache-php-app)
 set +x
